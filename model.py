46c0babcd38d94bc919a089edcb03dc32b1c7198
dyuing89 <87954296+dyuing89@users.noreply.github.com>
2022-01-17 13:30:01 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,2 @@
 #All radiographs were converted into numpyz files of x_train as a 224x224x3 pre-processed radiograph (image) and y_train as a survival array (groundtruth).
+#This section will be updated soon
